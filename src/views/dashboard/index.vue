46e1f3dee81ba75c1b0e55b87ab94bbdf9db2eb2
建力 <1879324764@qq.com>
2022-09-01 12:06:11 +0800

    base template

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -15,49 +15,8 @@
     <el-button type="warning" @click="count++">count is: {{ count }}</el-button>
     <el-button type="danger" @click="count++">count is: {{ count }}</el-button>
     <el-button type="info" @click="count++">count is: {{ count }}</el-button>
-    <br />
-    <el-input v-model="input" style="width: 200px; margin: 20px" />
-    <el-tag>Tag 1</el-tag>
-
-    <br />
-    <el-date-picker v-model="curDate" type="date" placeholder="Pick a day"></el-date-picker>
-
-    <a
-      href="https://element-plus.gitee.io/en-US/guide/theming.html"
-      style="color: var(--el-color-primary)"
-      target="_blank"
-    >
-      Doc of element-plus theme
-    </a>
-    <!--    <div class="dashboard-container mt-5">-->
-    <!--      <div class="dashboard-text">name: {{ username }}</div>-->
-    <!--    </div>-->
-    <!--    <div>update element-plus to 1.2.+, add svg icon, icon-font is abandoned</div>-->
-    <!--    <i class="el-icon-add-location">abandoned</i>-->
-    <!--    <div>update element-plus version to 2.0.1 release version the size options change to "large default small"</div>-->
-    <!--    <div class="mt-20px">global element svg
-     icon</div>-->
-    <!--    <ElSvgIcon name="Edit" :size="30" color="red" />-->
-    <!--    <div class="mt-20px">this is var from vite.config.js define</div>-->
-    <!--    <div>{{ showObj }},{{ showObjString }}</div>-->
-    <!--    -->
-    <div class="mt-5px text-18px font600">unocss</div>
-    <!-- bg-#fff   -->
-    <div class="mb-40px w-900px h-10px text-16px">
-      <div>
-        you can look
-        <el-link class="text-red" href="https://uno.antfu.me/" target="_blank">https://uno.antfu.me/</el-link>
-        to search you need such as "margin-left:10px" and then get the sortcut(ml-10px)
-      </div>
+    <h1><center>Python 测试运维自动化平台 </center> </h1>
     </div>
-
-    <div class="mt-3px mb1">
-      <div class="mb-1px">### How to migrate</div>
-      <div class="mb-1px">See how to migrate from **ElementUI** to **Element Plus** in our dedicated discussion:</div>
-      <div class="mb-1px">- For English: [#5658](https://github.com/element-plus/element-plus/discussions/5658)</div>
-      <div class="mb-1px">- 简体中文: [#5657](https://github.com/element-plus/element-plus/discussions/5657)</div>
-    </div>
-  </div>
 </template>
 <script setup>
 import { ElMessage } from 'element-plus'
